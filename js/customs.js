d8e445be9001137cfd105eb9cce92a9d0f96c0ac
TsurikViktor <tsurik.viktor.vladimirovich@gmail.com>
2021-08-22 23:13:22 +0300

    commit start

---
+++ js/customs.js
@@ -0,0 +1,35 @@
+const obj = {
+    prop: '42',
+    prop2: [8, 9, 10, {
+      beautifulPropertyName: 88,
+      'property with spaces': {
+        a: {
+          b: '',
+          c: {
+            someProperty: [{
+              'prop name': 'I am a smart programmer',
+            }],
+          },
+        },
+      },
+    }],
+    prop3: function() {
+      return {
+        baz: 'Hello',
+        bar: {
+          anotherBeautifulProp: [8, {
+            target : 'It was simple!',
+          }],
+        },
+      };
+    }
+  }
+  
+console.log(obj.prop2[3]['property with spaces'].a.c.someProperty[0]['prop name']); // I am a smart programmer
+console.log(obj.prop3.bar.anotherBeautifulProp[1].target); // It was simple
+  
+  // Используя операторы . и [] получить значение свойств 'target' и 'prop name' 
+  // у объекта записанного в переменную 'obj'.
+  // Вывести результат в консоль:
+  // console.log(obj ваш поезд здесь); // I am a smart programmer
+  // console.log(obj ваш поезд здесь); // It was simple
